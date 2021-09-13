9801f8a3c540b9cdcef21c734b5f827c9f129a96
Viktor Holovin <holovin@mail.com>
2021-09-13 14:54:14 +0200

    Create app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cinemapi
 Python script for generate json from Cinema folder
+
+## Installation guide
+```bash
+git clone https://github.com/holovin777/cinemapi.git
+cd cinemapi
+python cynemapi.py
+```
---
+++ cinemapi.py
@@ -0,0 +1,18 @@
+import os
+
+folder_path = input("Enter folder path (ex. /home/user/Cinema): ")
+api_url = input("Enter api url (ex. https://api.example.org): ")
+id = 0
+movies = []
+for file in os.walk(folder_path):
+    items = file[2]
+    for item in items:
+        if item.endswith('.mp4'):
+            movie_path = file[0]+"/"+item
+            movie_url = movie_path.replace(folder_path, api_url)
+            movie = {"id": id, "name": item, "movie_path": movie_path, "movie_url": movie_url}
+            movies.append(movie)
+            id = id+1
+f = open(folder_path+"/"+"index.html", "w")
+f.write(str(movies))
+f.close()
